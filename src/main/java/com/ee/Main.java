b9f9bb66a5cd9bbefc9ffbebbe178e159f912089
solnze111 <solnzemy@outlook.com>
2024-04-19 15:35:57 +0200

    ini project

---
+++ src/main/java/com/ee/Main.java
@@ -0,0 +1,12 @@
+package com.ee;
+
+import com.ee.demo.Greeting;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Greeting gr = new Greeting();
+		System.out.println(gr.sayHello("Pasha"));
+	}
+
+}
---
+++ src/main/java/com/ee/demo/Greeting.java
@@ -0,0 +1,9 @@
+package com.ee.demo;
+
+
+public class Greeting {
+	public String sayHello(String name) {
+		return "Hello " + name;
+	}
+
+}
